69de240613af337542bec8d4cce09378aba653c6
LukasWyver <lucas_sin5@hotmail.com>
2021-07-13 22:14:47 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,48 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  body {
+    font-family: sans-serif;
+    background-color: #d9e6f6;
+  }
+
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display:block;
   }
 `
 
 const theme = {
   colors: {
     primary: '#0070f3',
+    backgroundSecondary: '#F1F9FE',
+    backgroundDefault: '#D9E6F6',
+
+    primaryText:'#2E7BB4',
+    secondaryText:'#388BB0',
+    tertiaryText:'#2F4A71',
+
+    primaryElement:'#6F92BB',
+    anotherElement:'#5579A1',
+
+    gray1:'#333333',
+    gray2:'#5A5A5A',
+    gray3:'#999999',
+    gray4:'#C5C6CA',
+    gray5:'#F4F4F4',
   },
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,67 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+const Box = styled.div `
+    background-color: #ffffff;
+    border-radius: 8px;
+`;
+
+const MainGrid = styled.main `
+    width: 100%;
+    grid-gap: 10px;
+    margin-left: auto;
+    margin-right: auto;
+    max-width:500px;
+    padding: 16px;
+    
+    .profileArea {
+      display: none;
+        @media(min-width:860px) {
+          display: block;
+        }
+    }
+
+    @media(min-width: 860px){
+      max-width: 1110px;
+      display: grid;
+      grid-template-areas: 
+        "profileArea welcomeArea profileRelationsArea";
+      grid-template-columns: 160px 1fr 312px;
+    }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return(
+
+    <MainGrid>
+
+      <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+        <Box>
+          <img src="https://github.com/lukaswyver.png" />
+        </Box>
+      </div>
+
+      <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+        <Box>
+          Bem vindos
+        </Box>
+      </div>
+      
+      <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <Box>
+          Favoritos
+        </Box>
+
+        <Box>
+          Comunidades
+        </Box>
+      </div>
+      
+    </MainGrid>
+
+    );
 }
